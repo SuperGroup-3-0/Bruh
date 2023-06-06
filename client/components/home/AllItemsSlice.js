506f234de6397bb6fcb8b158bdc5faed75c9ca97
K-E_Becker <krstn.becker@gmail.com>
2023-06-06 16:43:25 -0400

    all item component/slice

---
+++ client/components/home/AllItemsSlice.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// Thunk
+export const fetchAllItemsAsync = createAsyncThunk(
+  "allItems/fetchAllItems",
+  async () => {
+    try {
+      const response = await axios.get("/api/items");
+      return response.data;
+    } catch (error) {
+      console.log("error:", error);
+      throw error;
+    }
+  }
+);
+
+// Slice
+const AllItemsSlice = createSlice({
+  name: "allItems",
+  initialState: {
+    items: [],
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllItemsAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAllItemsAsync.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.items = action.payload;
+      })
+      .addCase(fetchAllItemsAsync.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default AllItemsSlice.reducer;
