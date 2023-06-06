f86c574f58069128533f2d42a02f5edcf528a014
Nataliia Kazik <nataliia.kazik@gmail.com>
2023-06-06 13:44:49 -0700

    added Sequelize models, dummy data and seed function

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -13,7 +13,11 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
-  }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: "https://static.vecteezy.com/system/resources/previews/008/442/086/original/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
+}
 })
 
 module.exports = User
