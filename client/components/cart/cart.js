56730d4813db428335273d11339aff88bb77b541
K-E_Becker <krstn.becker@gmail.com>
2023-06-13 09:18:37 -0400

    memory fail was fixed

--- client/components/cart/cart.js
+++ client/components/cart/cart.js
@@ -93,6 +93,7 @@ const Cart = () => {
     setShippingMethod(event.target.value);
   };
 
+  //logged-in local storage
   useEffect(() => {
     if (isLoggedIn) {
       // Save cart to local storage whenever it changes
@@ -103,9 +104,41 @@ const Cart = () => {
   useEffect(() => {
     // Retrieve cart from local storage on component mount
     const savedCart = localStorage.getItem("cart");
-    if (savedCart && !isLoggedIn) {
+    if (savedCart && isLoggedIn) {
       const parsedCart = JSON.parse(savedCart);
       dispatch(setCart(parsedCart));
+    } else {
+      // Clear cart when user is not logged in
+      dispatch(setCart({ cartItems: [] }));
+    }
+  }, [dispatch, isLoggedIn]);
+
+  // Guest cart session storage
+  useEffect(() => {
+    if (!isLoggedIn) {
+      // Save cart to session storage with expiration time of 1 hour
+      sessionStorage.setItem("cart", JSON.stringify(cart));
+      const expirationTime = new Date().getTime() + 3600000; // 1 hour
+      sessionStorage.setItem("cartExpiration", expirationTime.toString());
+    }
+  }, [cart, isLoggedIn]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      // Retrieve cart from session storage on component mount
+      const savedCart = sessionStorage.getItem("cart");
+      const expirationTime = sessionStorage.getItem("cartExpiration");
+      if (savedCart && expirationTime) {
+        const currentTime = new Date().getTime();
+        if (currentTime <= parseInt(expirationTime)) {
+          const parsedCart = JSON.parse(savedCart);
+          dispatch(setCart(parsedCart));
+        } else {
+          sessionStorage.removeItem("cart");
+          sessionStorage.removeItem("cartExpiration");
+          dispatch(setCart({ cartItems: [] }));
+        }
+      }
     }
   }, [dispatch, isLoggedIn]);
 
@@ -122,83 +155,179 @@ const Cart = () => {
           ? `${username} Cart - ${currentMonth}/${currentDay}/${currentYear}`
           : `Guest Cart - ${currentMonth}/${currentDay}/${currentYear}`}
       </h2>
-      {cart.cartItems.length === 0 ? (
-        <div className="cartEmpty">
-          <p>You have no items in your cart!</p>
+      {isLoggedIn ? (
+        <div>
+          {cart.cartItems.length === 0 ? (
+            <div className="cartEmpty">
+              <p>You have no items in your cart!</p>
+            </div>
+          ) : (
+            <div>
+              <div className="cart-items">
+                {cart.cartItems.map((cartItem, index) => (
+                  <div className="cart-item" key={cartItem.id + index}>
+                    <div className="cart-product">
+                      <img
+                        src={cartItem.imageUrl}
+                        style={{ width: "300px", height: "300px" }}
+                      />
+                      <div>Item: {cartItem.name}</div>
+                      <p>Price: ${cartItem.price.toFixed(2)} USD</p>
+                      <div className="quantity-container">
+                        <span>Quantity: </span>
+                        <button
+                          onClick={() => handleDecreaseQuantity(cartItem.id)}
+                          disabled={cartItem.quantity === 1}
+                        >
+                          -
+                        </button>
+                        <span>{cartItem.quantity}</span>
+                        <button
+                          onClick={() => handleIncreaseQuantity(cartItem.id)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <p>
+                      Item Total: ${calculateItemTotal(cartItem).toFixed(2)} USD
+                    </p>
+                    <button onClick={() => handleDeleteItem(cartItem.id)}>
+                      Delete
+                    </button>
+                  </div>
+                ))}
+              </div>
+              <div>
+                <p>Items in Cart: {calculateTotalItems()}</p>
+                <p>Cart Total: ${calculateTotalItemCost().toFixed(2)} USD</p>
+                <br />
+                <p>Shipping Method:</p>
+                <div style={{ marginLeft: "20px" }}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="usps-ground"
+                      checked={shippingMethod === "usps-ground"}
+                      onChange={handleShippingMethodChange}
+                    />
+                    USPS Ground: $10.85 USD
+                  </label>
+
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="usps-priority"
+                      checked={shippingMethod === "usps-priority"}
+                      onChange={handleShippingMethodChange}
+                    />
+                    USPS Priority: $14.09 USD
+                  </label>
+
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="usps-overnight"
+                      checked={shippingMethod === "usps-overnight"}
+                      onChange={handleShippingMethodChange}
+                    />
+                    USPS Overnight: $49.45 USD
+                  </label>
+                </div>
+                <br />
+                <p>Sales Tax: ${calculateSalesTax().toFixed(2)} USD</p>
+                <p>Order Total: ${calculateOrderTotal().toFixed(2)} USD</p>
+              </div>
+              <button onClick={handleCheckout}>Checkout</button>
+            </div>
+          )}
         </div>
       ) : (
         <div>
-          <div className="cart-items">
-            {cart.cartItems.map((cartItem) => (
-              <div className="cart-item" key={cartItem.id}>
-                <div className="cart-product">
-                  <img src={cartItem.imageUrl} alt={cartItem.name} />
-                  <div>Item: {cartItem.name}</div>
-                  <p>Price: ${cartItem.price.toFixed(2)} USD</p>
-                  <div className="quantity-container">
-                    <span>Quantity: </span>
-                    <button
-                      onClick={() => handleDecreaseQuantity(cartItem.id)}
-                      disabled={cartItem.quantity === 1}
-                    >
-                      -
-                    </button>
-                    <span>{cartItem.quantity}</span>
-                    <button onClick={() => handleIncreaseQuantity(cartItem.id)}>
-                      +
+          {cart.guestCartItems.length === 0 ? (
+            <div className="cartEmpty">
+              <p>You have no items in your cart!</p>
+            </div>
+          ) : (
+            <div>
+              <div className="cart-items">
+                {cart.guestCartItems.map((cartItem, index) => (
+                  <div className="cart-item" key={cartItem.id + index}>
+                    <div className="cart-product">
+                      <img
+                        src={cartItem.imageUrl}
+                        style={{ width: "300px", height: "300px" }}
+                      />
+                      <div>Item: {cartItem.name}</div>
+                      <p>Price: ${cartItem.price.toFixed(2)} USD</p>
+                      <div className="quantity-container">
+                        <span>Quantity: </span>
+                        <button
+                          onClick={() => handleDecreaseQuantity(cartItem.id)}
+                          disabled={cartItem.quantity === 1}
+                        >
+                          -
+                        </button>
+                        <span>{cartItem.quantity}</span>
+                        <button
+                          onClick={() => handleIncreaseQuantity(cartItem.id)}
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
+                    <p>
+                      Item Total: ${calculateItemTotal(cartItem).toFixed(2)} USD
+                    </p>
+                    <button onClick={() => handleDeleteItem(cartItem.id)}>
+                      Delete
                     </button>
                   </div>
-                </div>
-                <p>
-                  Item Total: ${calculateItemTotal(cartItem).toFixed(2)} USD
-                </p>
-                <button onClick={() => handleDeleteItem(cartItem.id)}>
-                  Delete
-                </button>
+                ))}
               </div>
-            ))}
-          </div>
-          <div>
-            <p>Items in Cart: {calculateTotalItems()}</p>
-            <p>Cart Total: ${calculateTotalItemCost().toFixed(2)} USD</p>
-            <br />
-            <p>Shipping Method:</p>
-            <div style={{ marginLeft: "20px" }}>
-              <label>
-                <input
-                  type="checkbox"
-                  value="usps-ground"
-                  checked={shippingMethod === "usps-ground"}
-                  onChange={handleShippingMethodChange}
-                />
-                USPS Ground: $10.85 USD
-              </label>
+              <div>
+                <p>Items in Cart: {calculateTotalItems()}</p>
+                <p>Cart Total: ${calculateTotalItemCost().toFixed(2)} USD</p>
+                <br />
+                <p>Shipping Method:</p>
+                <div style={{ marginLeft: "20px" }}>
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="usps-ground"
+                      checked={shippingMethod === "usps-ground"}
+                      onChange={handleShippingMethodChange}
+                    />
+                    USPS Ground: $10.85 USD
+                  </label>
 
-              <label>
-                <input
-                  type="checkbox"
-                  value="usps-priority"
-                  checked={shippingMethod === "usps-priority"}
-                  onChange={handleShippingMethodChange}
-                />
-                USPS Priority: $14.09 USD
-              </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="usps-priority"
+                      checked={shippingMethod === "usps-priority"}
+                      onChange={handleShippingMethodChange}
+                    />
+                    USPS Priority: $14.09 USD
+                  </label>
 
-              <label>
-                <input
-                  type="checkbox"
-                  value="usps-overnight"
-                  checked={shippingMethod === "usps-overnight"}
-                  onChange={handleShippingMethodChange}
-                />
-                USPS Overnight: $49.45 USD
-              </label>
+                  <label>
+                    <input
+                      type="checkbox"
+                      value="usps-overnight"
+                      checked={shippingMethod === "usps-overnight"}
+                      onChange={handleShippingMethodChange}
+                    />
+                    USPS Overnight: $49.45 USD
+                  </label>
+                </div>
+                <br />
+                <p>Sales Tax: ${calculateSalesTax().toFixed(2)} USD</p>
+                <p>Order Total: ${calculateOrderTotal().toFixed(2)} USD</p>
+              </div>
+              <button onClick={handleCheckout}>Checkout</button>
             </div>
-            <br />
-            <p>Sales Tax: ${calculateSalesTax().toFixed(2)} USD</p>
-            <p>Order Total: ${calculateOrderTotal().toFixed(2)} USD</p>
-          </div>
-          <button onClick={handleCheckout}>Checkout</button>
+          )}
         </div>
       )}
     </div>
--- client/components/cart/cartslice.js
+++ client/components/cart/cartslice.js
@@ -1,5 +1,3 @@
-// cartslice.js
-
 import { createSlice } from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
@@ -8,14 +6,58 @@ const cartSlice = createSlice({
     cartItems: localStorage.getItem("cart")
       ? JSON.parse(localStorage.getItem("cart")).cartItems
       : [],
+    guestCartItems: localStorage.getItem("guestCartItems")
+      ? JSON.parse(localStorage.getItem("guestCartItems")).guestCartItems
+      : [],
   },
   reducers: {
     addToCart(state, action) {
-      state.cartItems.push({ ...action.payload, quantity: 1 }); // Set initial quantity to 1
+      const newItem = { ...action.payload, quantity: 1 };
+      if (localStorage.getItem("token")) {
+        // If token exists, add the item to cartItems only if it doesn't exist
+        const existingCartItem = state.cartItems.find(
+          (item) => item.id === newItem.id
+        );
+        if (!existingCartItem) {
+          state.cartItems.push(newItem);
+          // Update the local storage with the updated cartItems
+          localStorage.setItem(
+            "cart",
+            JSON.stringify({ cartItems: state.cartItems })
+          );
+        }
+      } else {
+        // If no token, add the item to guestCartItems only if it doesn't exist
+        const existingGuestCartItem = state.guestCartItems.find(
+          (item) => item.id === newItem.id
+        );
+        if (!existingGuestCartItem) {
+          state.guestCartItems.push(newItem);
+          // Update the local storage with the updated guestCartItems
+          localStorage.setItem(
+            "guestCartItems",
+            JSON.stringify({ guestCartItems: state.guestCartItems })
+          );
+        }
+      }
     },
     removeFromCart(state, action) {
       const itemId = action.payload;
       state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      // Update the local storage with the updated cart items
+      localStorage.setItem(
+        "cart",
+        JSON.stringify({ cartItems: state.cartItems })
+      );
+    },
+    removeFromCart(state, action) {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      // Update the local storage with the updated cart items
+      localStorage.setItem(
+        "cart",
+        JSON.stringify({ cartItems: state.cartItems })
+      );
     },
     updateCartItemQuantity(state, action) {
       const { itemId, newQuantity } = action.payload;
@@ -27,6 +69,11 @@ const cartSlice = createSlice({
         return item;
       });
       state.cartItems = updatedCartItems;
+      // Update the local storage with the updated cart items
+      localStorage.setItem(
+        "cart",
+        JSON.stringify({ cartItems: state.cartItems })
+      );
     },
     setCart(state, action) {
       state.cartItems = action.payload.cartItems;
@@ -36,4 +83,5 @@ const cartSlice = createSlice({
 
 export const { setCart, addToCart, removeFromCart, updateCartItemQuantity } =
   cartSlice.actions;
+
 export default cartSlice.reducer;
