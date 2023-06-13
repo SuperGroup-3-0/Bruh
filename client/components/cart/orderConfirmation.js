d11cf515c7c92514483dc766f5fee9103ae2833f
Nataliia Kazik <nataliia.kazik@gmail.com>
2023-06-13 15:33:00 -0700

    orderConfirmation directs to a page with random generated number

--- client/components/cart/orderConfirmation.js
+++ client/components/cart/orderConfirmation.js
@@ -15,11 +15,19 @@ export const OrderConfirmation = () => {
         return state.auth.me.username;
     });
 
+    const getRandomInt = (min, max) => {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+
     return (
     <div id="order-confirmation"> 
         <img src="https://img.freepik.com/free-vector/verified-concept-illustration_114360-5152.jpg?w=1480&t=st=1686677684~exp=1686678284~hmac=a00ec101474be46acd191f6fba0867bfdfe8c42879914613011102b481b46826" width="200" height="200" alt="Confirmation" />
         <h1>Thank you for your order, {username}!</h1>
-        <p>{order}</p>
+        <h2>Your order number is {
+              getRandomInt(34500, 25689065)
+              } </h2>
         <div>
         </div>
     </div>
